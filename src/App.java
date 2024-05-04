0243827d5f2b65ca666917b333fe8e5253bffc98
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-05-03 23:45:53 -0300

    Repeticao while e do while

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)  {
+        
+        int numero = 7, tentativas = 5;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (tentativas > 0) {
+            System.out.println("Digite um número entre 1 e 10:");
+            int numeroFornecido = scanner.nextInt();
+
+            if(numeroFornecido == numero){
+                System.out.println("Você acertou.");
+                return;
+            }else{
+                tentativas--;
+            }
+
+        }
+        scanner.close();
+
+    }
+}
