13c2ce510b8862ac7807863fc7f99dff428f92d0
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-05-10 23:10:11 -0300

    Utilizando o break

--- src/JumpStatements.java
+++ src/JumpStatements.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class JumpStatements {
     
     public static void main(String[] args) {
         //continue
-        for(int i = 1; i <= 50; i++){
-            if (i % 2 != 0) 
-                continue;
-                System.out.println(i);
+        // for(int i = 1; i <= 50; i++){
+        //     if (i % 2 != 0) 
+        //         continue;
+        //         System.out.println(i);
             
+        //}
+
+        //Break
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Digite um número");
+
+            int numero = scanner.nextInt();
+
+            if(numero == 1)
+            break;
         }
+        System.out.println("Ufa, acabou");
+        scanner.close();
     }
 }
