d0051e6f9ed79969af3cacc55d9367e678e4c15f
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-05-10 23:32:21 -0300

    Loops Aninhados

---
+++ src/NestedLoops.java
@@ -0,0 +1,10 @@
+public class NestedLoops {
+    
+    public static void main(String[] args) {
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++)
+            System.out.print(i + "-" + j + " ");
+        System.out.println();
+        }
+    }
+}
