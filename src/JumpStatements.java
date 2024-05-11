f77f9c341fde2a8490039c8e55962a50f5535e11
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-05-10 23:05:29 -0300

    Trabalhando com o continue

---
+++ src/JumpStatements.java
@@ -0,0 +1,12 @@
+public class JumpStatements {
+    
+    public static void main(String[] args) {
+        //continue
+        for(int i = 1; i <= 50; i++){
+            if (i % 2 != 0) 
+                continue;
+                System.out.println(i);
+            
+        }
+    }
+}
