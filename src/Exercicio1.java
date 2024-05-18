08249e501e4a46aa23d73bbd6077ff3cfe97e9d0
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-05-18 10:04:11 -0300

    Exercicio 1. Contando de1 a 10 com while

---
+++ src/Exercicio1.java
@@ -0,0 +1,16 @@
+
+/*
+ * Contando de 1 a 10
+Escreva um programa Java que imprime números de 1 à 10 utilizando 
+a estrutura de repetição while. O programa deve imprimir cada número em uma linha diferente.
+ */
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 10) {
+            System.err.println("Número :" + numero);
+            numero++;
+        }
+    }
+}
